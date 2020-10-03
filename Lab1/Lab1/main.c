c24c1173f2f97d6d2b68ab4fe87b4d509d7afde0
cyberFerret01 <60542526+cyberFerret01@users.noreply.github.com>
2020-10-03 22:17:19 +0100

    settingUp

---
+++ Lab1/Lab1/main.c
@@ -0,0 +1,19 @@
+/*
+ * Lab1.c
+ *
+ * Created: 03/10/2020 13:40:01
+ * Author : bear
+ */ 
+
+#include<avr/io.h>
+#define F_CPU 16000000UL
+#include"util/delay.h"
+int main(void)
+{
+	DDRB=0xFF;//make port B as output port
+	while(1){PORTB=0xFF;//make all pins HIGH
+		_delay_ms(1000);//wait 1 sec
+		PORTB=0x00;//make all pins LOW
+		_delay_ms(1000);//wait 1 sec
+		}}
+
