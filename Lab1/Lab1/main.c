65ae697f543b5ef963d7744d50e693ee294ee65e
cyberFerret01 <60542526+cyberFerret01@users.noreply.github.com>
2020-10-15 22:36:14 +0100

    the timer is now working

--- Lab1/Lab1/main.c
+++ Lab1/Lab1/main.c
@@ -9,16 +9,48 @@
 #define F_CPU 16000000UL
 #include"util/delay.h"
 const int inital = (9 + 18) * 100;
+//37.03 x10^-3
+//0.0370 x10^-6
 
 int main(void)
 {
-	DDRB=0xFF;//make port B as output port
+
+	//
+	DDRB=0xFF;
 	DDRB &= ~(1 << 5);
-	//5th
+	
 	while(1){
-		PORTB=0b111;
+		
+	OCR1A = 0x7A12;
+	//OCR1AL = 0xCB;
+	TCCR1A = 0;	//0b10;
+	TCCR1B =0b01101; //0x05;	//0b0101;
+	
+	while ((TIFR1&(1<<OCF1A) )==0){
+		
+	}
+		TCCR1B =0;
+		TIFR1 = 1<<OCF1A;
+		TIFR1 = 1<<TOV1;
+		/*
+		_delay_ms(inital);
+				PORTB=0b00;
+		for(int i = 0; i < 3; i++){
+			_delay_ms(inital);
+			PORTB |= (1 << i);
+			
+			*/
+		PORTB ^= 1<<3;
+		}
+		
+
+		
+		
+/*
 		_delay_ms(inital);
 		PORTB=0b00;
 		_delay_ms(inital);
-		}}
+		*/
+		
+}
 
