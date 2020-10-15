816171cd1d67f59b1a87ea14482f533d1320ded4
cyberFerret01 <60542526+cyberFerret01@users.noreply.github.com>
2020-10-15 15:02:23 +0100

    c portion with sotware delay

--- Lab1/Lab1/main.c
+++ Lab1/Lab1/main.c
@@ -8,12 +8,17 @@
 #include<avr/io.h>
 #define F_CPU 16000000UL
 #include"util/delay.h"
+const int inital = (9 + 18) * 100;
+
 int main(void)
 {
 	DDRB=0xFF;//make port B as output port
-	while(1){PORTB=0xFF;//make all pins HIGH
-		_delay_ms(1000);//wait 1 sec
-		PORTB=0x00;//make all pins LOW
-		_delay_ms(1000);//wait 1 sec
+	DDRB &= ~(1 << 5);
+	//5th
+	while(1){
+		PORTB=0b111;
+		_delay_ms(inital);
+		PORTB=0b00;
+		_delay_ms(inital);
 		}}
 
