3e2fea6d75bb271c523512399e0db4cf9ba7447b
cyberFerret01 <60542526+cyberFerret01@users.noreply.github.com>
2020-10-15 23:42:17 +0100

    Task 1 done!!

--- Lab1/Lab1/main.c
+++ Lab1/Lab1/main.c
@@ -2,10 +2,12 @@
  * Lab1.c
  *
  * Created: 03/10/2020 13:40:01
- * Author : bear
+ * Author : Robert Nicoll (rn37)
  */ 
 
 #include<avr/io.h>
+#include<avr/interrupt.h>
+
 #define F_CPU 16000000UL
 #include"util/delay.h"
 const int inital = (9 + 18) * 100;
@@ -18,33 +20,13 @@ int main(void)
 	//
 	DDRB=0xFF;
 	DDRB &= ~(1 << 5);
-	
-	while(1){
-		
 	OCR1A = 0x7A12;
-	//OCR1AL = 0xCB;
-	TCCR1A = 0;	//0b10;
-	TCCR1B =0b01101; //0x05;	//0b0101;
 	
-	while ((TIFR1&(1<<OCF1A) )==0){
+	
+		EIMSK = (1 << INT0);
+		sei();
 		
-	}
-		TCCR1B =0;
-		TIFR1 = 1<<OCF1A;
-		TIFR1 = 1<<TOV1;
-		/*
-		_delay_ms(inital);
-				PORTB=0b00;
-		for(int i = 0; i < 3; i++){
-			_delay_ms(inital);
-			PORTB |= (1 << i);
-			
-			*/
-		PORTB ^= 1<<3;
-		}
-		
-
-		
+		while(1){}
 		
 /*
 		_delay_ms(inital);
@@ -52,5 +34,28 @@ int main(void)
 		_delay_ms(inital);
 		*/
 		
+
 }
+ISR(INT0_vect){
+	
+	for(int i = 0; i < 3; i++){
+			
+		PORTB |= (1 << i);
+			
+			//OCR1A = 0x7A12;
+			TCCR1A = 0;
+			TCCR1B =0b01101;
+					
+			while ((TIFR1&(1<<OCF1A) )==0){
+						
+			}
+			TCCR1B =0;
+			TIFR1 = 1<<OCF1A;
+
+	}
+	
+	PORTB=0b00;
+	
+}
+
 
