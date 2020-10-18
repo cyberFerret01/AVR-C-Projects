e7ae19f1838f058e8c9c882dc24fb98ca1e4cdf2
cyberFerret01 <60542526+cyberFerret01@users.noreply.github.com>
2020-10-18 14:48:22 +0100

    Lab done and submitted

--- Lab1Task1/Lab1Task1/main.asm
+++ Lab1Task1/Lab1Task1/main.asm
@@ -13,31 +13,32 @@
 ;	
 ;	0xA4CA OR 0b1010010011001010
 ;
-		 LDI R23, 0b10100100 ; load R23 with calculated delay value
-		 LDI R24, 0b11001010
-		 LDI R16, 0b00000111
-		 OUT DDRB, R16 ;PB3 as an output  
+		 LDI R23, 0b11000011 ;load R23 with upper 8 bits of calculated delay value
+		 STS OCR1AH,R23      ;load r23 into OCR1AH (HIGH must be written below OCR1AL)
+		 LDI R23, 0b01010000 ;load R32 with the lower 8 bits of calculated delay values
+		 STS OCR1AL,R23
 
-		 LDI R16,0;111 ;R16 = 0x08 
+		 LDI R16, 0b00000111
+		 OUT DDRB, R16 ;PB0,PB1 and PB3 as outputs
+
+		 LDI R16,0
 		 LDI R17,0b1
     
-		 OUT PORTB,R16
+		 OUT PORTB,R16 ;initialize all lamps as off
 		 		     
-		 STS OCR1AH,R23      
-		 STS OCR1AL,R24
+
 BEGIN:	 LDI R20,0x00  
 		 STS TCCR1A,R20       
 		 LDI R20, 0b01101  
-		 STS TCCR1B,R20     
-AGAIN:   SBIS     TIFR1,OCF1A
+		 STS TCCR1B,R20   ;start ctc timer in 1/1024 mode  
+AGAIN:   SBIS     TIFR1,OCF1A	;wait till timer reaches threshold
 		 RJMP AGAIN  
 		 LDI R20,0x00 
-		 STS TCCR1B,R20  
+		 STS TCCR1B,R20  ;Stop the timer
 		 LDI R20,1<<OCF1A 
 		 OUT TIFR1,R20   
 
-		 OR R16,R17
-		 LSL R17
-		 OUT PORTB,R16  
-	
+		 OR R16,R17	;do a or operation on R17 and R16 and save into R16 (so for the 1st look R16 goes from 0 to 0b00000001)
+		 LSL R17	;shift r17 to the lest one to allow the lamps to increment
+		 OUT PORTB,R16  ;set the high bits in R16 to the PORTB to light the lamps
 		 RJMP  BEGIN
