3f409f08b6e322a435de044f640e9a9e221fc4fb
cyberFerret01 <60542526+cyberFerret01@users.noreply.github.com>
2020-10-26 17:26:48 +0000

    initalComitForLab2

---
+++ lab2Task1/lab2Task1/main.c
@@ -0,0 +1,18 @@
+/*
+ * lab2Task1.c
+ *
+ * Created: 26/10/2020 17:24:59
+ * Author : bear
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
