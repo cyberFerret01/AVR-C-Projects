b3ee821e43d06931f4979731d573ca0300d9b92b
cyberFerret01 <60542526+cyberFerret01@users.noreply.github.com>
2020-10-26 22:23:11 +0000

    pwmExampleDoneAndPreScalerCalculated

--- lab2Task1/lab2Task1/main.c
+++ lab2Task1/lab2Task1/main.c
@@ -2,17 +2,35 @@
  * lab2Task1.c
  *
  * Created: 26/10/2020 17:24:59
- * Author : bear
+ * Author : Robert Nicoll rn37
  */ 
-
+#define F_CPU 16000000UL
 #include <avr/io.h>
+#include <util/delay.h>
 
 
 int main(void)
 {
+	
+	DDRB = 0x20;
+	//5 high
+	
     /* Replace with your application code */
-    while (1) 
+ /*   while (1) 
     {
+		PORTB |= (1<<5);
+		_delay_ms(1000);
+		PORTB &= ~(1<<5);
+		_delay_ms(1000);
     }
+	*/
+ DDRD|=(1<<2)|(1<<3);
+ PORTD=0x08;//OCR0A PWM, set PortD.6 output
+ DDRD|=(1<<6);//set OCR0A PWM, fast PWM, no prescaler, non-inverted
+ TCCR0B=0b101;
+ TCCR0A=0x83;
+ OCR0A=0xff;
+ while(1);
+ return 0;
 }
 
