22a91c60932c89b7092d0329b916be8f93897892
cyberFerret01 <60542526+cyberFerret01@users.noreply.github.com>
2020-10-27 01:35:18 +0000

    endOfDayComit

--- buttons/buttons/main.c
+++ buttons/buttons/main.c
@@ -7,12 +7,27 @@
 
 #include <avr/io.h>
 
+#define F_CPU 16000000UL
+#include"util/delay.h"
 
 int main(void)
 {
+	DDRB = 0x20;
+	PORTD = 0xFF;
     /* Replace with your application code */
     while (1) 
     {
-    }
-}
+		_delay_ms(1000);
+	//	if (PIND == 0b100)
+		//{
+			PORTB &= ~(1 << 5);
+			_delay_ms(1000);
+		//}else{
+		
+		PORTB |= (1 << 5);
+		
+
+	//	}
+		}
+		}
 
