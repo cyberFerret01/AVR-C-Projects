144ceb2173c3ab43319fab6e3aea7dab313d9d32
cyberFerret01 <60542526+cyberFerret01@users.noreply.github.com>
2020-10-27 00:04:45 +0000

    pwmWidthsDone
    having issues getting buttons working though

--- lab2Task1/lab2Task1/main.c
+++ lab2Task1/lab2Task1/main.c
@@ -27,10 +27,105 @@ int main(void)
  DDRD|=(1<<2)|(1<<3);
  PORTD=0x08;//OCR0A PWM, set PortD.6 output
  DDRD|=(1<<6);//set OCR0A PWM, fast PWM, no prescaler, non-inverted
- TCCR0B=0b101;
+ 
  TCCR0A=0x83;
- OCR0A=0xff;
- while(1);
+
+int buttonState = 0;	
+
+ while(1){
+	 
+	 switch(buttonState){
+		 case 0:
+		 //0
+		 //0
+		 OCR0A=0x00;
+		 PORTB |= (1<<5);
+		 break;
+		 
+		 case 1:
+		 //25
+		 //63 -3f
+		 OCR0A=0x3f;
+		 break;
+		 
+		 case 2:
+		 //62.5
+		 //159 - 9f
+		 OCR0A=0x9f;
+		 break;
+		 
+		 case 3:
+		 //87.5
+		 //223 - df
+		 OCR0A=0xdf;
+		 
+		 break;
+	 }
+	 TCCR0B=0b101;
+	 
+	 buttonState++;
+	 
+	 _delay_ms(2000);
+	 
+	 
+
+
+	/* 
+	 if(PINB != buttonState){
+		 _delay_ms(10);
+		 if (PINB != buttonState)
+		 {
+		 
+			 buttonState = PINB;
+			 TCCR0B=0x00;
+			 _delay_ms(1000);
+		 
+			 switch(PINB){
+				 case 0b10:
+				 //0
+				 //0
+				 OCR0A=0x00;
+				 PORTB |= (1<<5);
+					 break;
+				 
+				 case 0b100:
+				 //25
+				 //63 -3f
+				 OCR0A=0x3f;
+					 break;
+				 
+				 case 0b1000:
+				 //62.5
+				 //159 - 9f
+				 OCR0A=0x9f;
+					 break;
+				 
+				 case 0b10000:
+				 //87.5
+				 //223 - df
+				 OCR0A=0xdf;
+				 
+					 break;
+			 }
+		 TCCR0B=0b101;
+		 }
+	 }
+	 */
+ }
+ }
+ 
+ 
+	/* 
+	TCCR0B=0b101;
+	OCR0A=0x10;
+	_delay_ms(10000);
+	TCCR0B = 0x00;
+	_delay_ms(1000);
+	TCCR0B=0b101;
+	OCR0A=0xBF;
+	_delay_ms(10000);
+	 };
  return 0;
-}
+ */
+
 
