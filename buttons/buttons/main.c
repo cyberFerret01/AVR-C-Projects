22344bd002ffcea6aaf6b142d9fbd14d86a20aab
cyberFerret01 <60542526+cyberFerret01@users.noreply.github.com>
2020-10-27 00:06:02 +0000

    buttonsInNewProject
    trying to avoid spag bol in lab code

---
+++ buttons/buttons/main.c
@@ -0,0 +1,18 @@
+/*
+ * buttons.c
+ *
+ * Created: 27/10/2020 00:05:14
+ * Author : bear
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
